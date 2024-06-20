335285c13fe38e0729e94104760213a0b7a74f38
xtorres10 <xiomara.torresg01@gmail.com>
2024-06-20 09:20:19 +0200

    Fix calculate_fare function

--- model.py
+++ model.py
@@ -14,16 +14,16 @@ class Taximetro:
         now = time.time()
         time_elapsed = now - self.last_status_change
         if self.in_movement:
-            self.fare_total += time_elapsed * self.fare_movement
+            self.fare_total += round(time_elapsed * self.fare_stop, 2)
         else:
-            self.fare_total += time_elapsed * self.fare_stop
+            self.fare_total += round(time_elapsed * self.fare_movement, 2)
         self.last_status_change = now
 
     def start(self):
         print("Comenzar la carrera.")
         self.start_road = True
         self.last_status_change = time.time()
-        self.calculate_fare()
+        #self.calculate_fare()
 
     def stop(self):
         print("El taxi se ha detenido. ")
@@ -37,12 +37,13 @@ class Taximetro:
             print(f"El taxi esta en movimiento.")
     
     def finish_road(self):
+        self.in_movement = not self.in_movement
         self.calculate_fare()
+        self.start_road = False
         print(f"La carrera ha terminado. El total a cobrar es: {self.fare_total:.2f} euros.")
         return self.fare_total
     
     def clear(self):
-        self.start_road = False
         self.last_status_change= None
         self.fare_total = 0
         self.in_movement = False
