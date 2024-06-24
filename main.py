12a1d5896c88315fe7ddec88f249fbcb7fec3163
GreciaLH <glandazuriherrera@gmail.com>
2024-06-24 03:01:56 +0200

    Actualizar dev

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import logging
 from model import Taximetro
 import time
 
-#Función main():
-#Crea una instancia de la clase Taximetro.
-#Imprime las instrucciones para el usuario.
-#Entra en un bucle infinito para leer comandos del usuario y llamar a los métodos correspondientes del taxímetro.
+logging.basicConfig(filename='taximetro.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+
 def main():
+    logging.info("Inicio del programa Taximetro Digital.")
     taximetro = Taximetro()
 
     print("Bienvenido al Taxímetro Digital!")
@@ -15,7 +16,6 @@ def main():
           - continuar
           - finalizar
             con ellos puede usar el programa.\n''')
-
 
     while True:
         comando = input("Ingrese un comando: ")
@@ -30,7 +30,7 @@ def main():
             taximetro.clear()
         else:
             print("Comando inválido. Intente de nuevo.")
-
+            logging.warning("Comando inválido ingresado.")
 
 if __name__ == "__main__":
     main()
--- model.py
+++ model.py
@@ -1,52 +1,53 @@
+import logging
 import time
 
 class Taximetro:
     fare_movement = 0.05  # tarifa en movimiento en centimos de euro por segundo
-    fare_stop = 0.02 #tarifa en reposo de centimos en euro por segundo
+    fare_stop = 0.02  # tarifa en reposo de centimos en euro por segundo
 
     def __init__(self):
         self.start_road = False
-        self.last_status_change= None
-        self.fare_total = 0 #ira aumentando segun se mueva o este en espera despues de iniciar la carrera
+        self.last_status_change = None
+        self.fare_total = 0  # ira aumentando segun se mueva o este en espera despues de iniciar la carrera
         self.in_movement = False
+        logging.info("Taximetro inicializado.")
 
     def calculate_fare(self):
         now = time.time()
         time_elapsed = now - self.last_status_change
         if self.in_movement:
             self.fare_total += round(time_elapsed * self.fare_stop, 2)
+            logging.info(f"Calculando tarifa en reposo: {self.fare_total}")
         else:
             self.fare_total += round(time_elapsed * self.fare_movement, 2)
+            logging.info(f"Calculando tarifa en movimiento: {self.fare_total}")
         self.last_status_change = now
 
     def start(self):
-        print("Comenzar la carrera.")
+        logging.info("Comenzar la carrera.")
         self.start_road = True
         self.last_status_change = time.time()
-        #self.calculate_fare()
 
     def stop(self):
-        print("El taxi se ha detenido. ")
+        logging.info("El taxi se ha detenido.")
         self.in_movement = False
         self.calculate_fare()
 
-    def continue_road(self):   
+    def continue_road(self):
         if self.start_road:
             self.in_movement = True
             self.calculate_fare()
-            print(f"El taxi esta en movimiento.")
-    
+            logging.info("El taxi esta en movimiento.")
+
     def finish_road(self):
         self.in_movement = not self.in_movement
         self.calculate_fare()
         self.start_road = False
-        print(f"La carrera ha terminado. El total a cobrar es: {self.fare_total:.2f} euros.")
+        logging.info(f"La carrera ha terminado. El total a cobrar es: {self.fare_total:.2f} euros.")
         return self.fare_total
-    
+
     def clear(self):
-        self.last_status_change= None
+        self.last_status_change = None
         self.fare_total = 0
         self.in_movement = False
-
-
-       
+        logging.info("Taximetro reiniciado.")
